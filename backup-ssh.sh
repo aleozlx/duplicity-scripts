f34baafb5810c617195d1316688ecf0d4d6a0831
Alex Yang <aleozlx@gmail.com>
2017-06-03 13:38:26 -0500

    check connection

--- backup-ssh.sh
+++ backup-ssh.sh
@@ -3,6 +3,8 @@
 # sudo apt install libssl-dev
 # sudo pip2 install paramiko
 
+if ssh research -q exit ; then
+
 duplicity --verbosity notice \
 	--full-if-older-than 60D \
 	--num-retries 3 \
@@ -11,3 +13,10 @@ duplicity --verbosity notice \
 	--log-file ./duplicity.log \
 	--exclude-filelist ./ignore \
 	~/ rsync://research/Backup/CyberpowerPC
+
+else
+
+echo "Can't establish connection to backup server."
+
+fi
+
